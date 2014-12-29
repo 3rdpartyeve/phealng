d6053879eaecc9a381c2c49ddebc10014cab0fb0
Peter Petermann <peter.petermann@gameforge.com>
2014-12-29 12:54:06 +0100

    code style fixes

--- lib/Pheal/Log/FileStorage.php
+++ lib/Pheal/Log/FileStorage.php
@@ -37,16 +37,19 @@ class FileStorage implements CanLog
 {
     /**
      * path where to store the logs
+     *
      * @var string
      */
     protected $basepath;
     /**
      * saved startTime to measure the response time
+     *
      * @var float
      */
     protected $startTime = 0.0;
     /**
      * save the response time after stop() or log()
+     *
      * @var float
      */
     protected $responseTime = 0.0;
@@ -54,6 +57,7 @@ class FileStorage implements CanLog
      * various options for the filecache
      * valid keys are: access_log, error_log, access_format, error_format, trancate_apikey, umask, umask_directory
      * both logfiles are getting passed to strftime() in the case you want to crate a simple logrotate
+     *
      * @var array
      */
     protected $options = array(
@@ -68,15 +72,16 @@ class FileStorage implements CanLog
 
     /**
      * construct
+     *
      * @param bool|string $basepath optional string on where to store files, defaults to ~/.pheal/cache/
      * @param array $options optional config array, valid keys are: delimiter, umask, umask_directory
      */
     public function __construct($basepath = false, $options = array())
     {
         if (!$basepath) {
-            $this->basepath = getenv('HOME') . "/.pheal/log/";
+            $this->basepath = getenv('HOME')."/.pheal/log/";
         } else {
-            $this->basepath = (string) $basepath;
+            $this->basepath = (string)$basepath;
         }
 
         // add options
@@ -96,16 +101,19 @@ class FileStorage implements CanLog
 
     /**
      * returns current microtime
+     *
      * @return float
      */
     protected function getmicrotime()
     {
         list($usec, $sec) = explode(" ", microtime());
+
         return ((float)$usec + (float)$sec);
     }
 
     /**
      * logs request api call including options
+     *
      * @param string $scope
      * @param string $name
      * @param array $opts
@@ -133,6 +141,7 @@ class FileStorage implements CanLog
             ),
             FILE_APPEND
         );
+
         return true;
     }
 
@@ -152,6 +161,7 @@ class FileStorage implements CanLog
 
     /**
      * create a filename to use (type can be access_log or error_log)
+     *
      * @param string $type
      * @return string
      */
@@ -170,18 +180,20 @@ class FileStorage implements CanLog
         }
 
         // create full logfile name (incl. name passing through strftime
-        $fullFilename = $this->basepath . strftime($filename);
+        $fullFilename = $this->basepath.strftime($filename);
 
         // create the logfile of not existing
         if (!file_exists($fullFilename)) {
             file_put_contents($fullFilename, '');
             chmod($fullFilename, $this->options['umask']);
         }
+
         return $fullFilename;
     }
 
     /**
      * returns formatted url for logging
+     *
      * @param string $scope
      * @param string $name
      * @param array $opts
@@ -190,23 +202,23 @@ class FileStorage implements CanLog
     protected function formatUrl($scope, $name, $opts)
     {
         // create url
-        $url = Config::getInstance()->api_base . $scope . '/' . $name . ".xml.aspx";
+        $url = Config::getInstance()->api_base.$scope.'/'.$name.".xml.aspx";
 
         // truncacte apikey for log safety
         if ($this->options['truncate_apikey'] && count($opts)) {
             if (isset($opts['apikey'])) {
-                $opts['apikey'] = substr($opts['apikey'], 0, 16) . "...";
+                $opts['apikey'] = substr($opts['apikey'], 0, 16)."...";
             }
             if (isset($opts['vCode'])) {
-                $opts['vCode'] = substr($opts['vCode'], 0, 16) . "...";
+                $opts['vCode'] = substr($opts['vCode'], 0, 16)."...";
             }
         }
 
         // add post data
         if (Config::getInstance()->http_post) {
-            $url .= " DATA: " . http_build_query($opts, '', '&');
+            $url .= " DATA: ".http_build_query($opts, '', '&');
         } elseif (count($opts)) { // add data to url
-            $url .= '?' . http_build_query($opts, '', '&');
+            $url .= '?'.http_build_query($opts, '', '&');
         }
 
         return $url;
@@ -214,6 +226,7 @@ class FileStorage implements CanLog
 
     /**
      * logs failed request api call including options and error message
+     *
      * @param string $scope
      * @param string $name
      * @param array $opts
@@ -246,6 +259,7 @@ class FileStorage implements CanLog
             ),
             FILE_APPEND
         );
+
         return true;
     }
 }
